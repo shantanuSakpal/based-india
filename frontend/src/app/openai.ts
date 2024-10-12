67401f5ba1a31ff170b6a9b8ea16b8e4e0602630
shantanuSakpal <shantanuesakpal1420@gmail.com>
2024-10-12 17:00:05 +0530

    open ai api key update

--- frontend/src/app/openai.ts
+++ frontend/src/app/openai.ts
@@ -2,12 +2,6 @@ import OpenAI from "openai";
 
 const NEXT_PUBLIC_OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
 
-if (!NEXT_PUBLIC_OPENAI_API_KEY) {
-  throw new Error(
-    "NEXT_PUBLIC_OPENAI_API_KEY is not set in the environment variables"
-  );
-}
-
 export const openai = new OpenAI({
   apiKey: NEXT_PUBLIC_OPENAI_API_KEY,
 });
