05f418797448074ad2ebfd0841cd10a194f88cb7
shantanuSakpal <shantanuesakpal1420@gmail.com>
2024-10-20 14:12:00 +0530

    bug fix

--- frontend/src/components/WalletConnectButton.tsx
+++ frontend/src/components/WalletConnectButton.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { useWeb3Modal } from "@web3modal/wagmi/react";
-import { useAccount, useDisconnect } from "wagmi";
-import { Button, ButtonGroup } from "@nextui-org/button";
 import WalletWrapper from "./WalletWrapper";
 
-export default function WalletConnectButton({text, className}: {text: string, className: string}) {
+export default function WalletConnectButton({
+  text,
+  className,
+}: {
+  text: string;
+  className: string;
+}) {
   return (
     <WalletWrapper
-      className= {className || "min-w-[90px] bg-blue-500 hover:bg-blue-600"}
+      className={className || "min-w-[90px] bg-blue-500 hover:bg-blue-600"}
       text={text || "Connect Wallet"}
       withWalletAggregator={true}
     />
